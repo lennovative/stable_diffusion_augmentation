5a88ad614acdb4d01226a0a89c1238db4e6b1221
Lennart <mailtolennart@gmx.de>
2026-06-01 22:41:09 +0200

    blur fix and new settings

--- sd_editing/editing.py
+++ sd_editing/editing.py
@@ -262,7 +262,7 @@ def reconstruct_ddim_with_attention_restoration(
                 main_mask = _cast(main_mask)
 
             main_mask_bin = main_mask.clone()
-            main_mask = base_mask_soft * main_mask_bin
+            main_mask = main_mask_bin if not recon_blur_k else base_mask_soft * main_mask_bin
     else:
         base_mask_soft = _cast(base_mask_soft)
         main_mask_bin = _cast(main_mask_bin)
